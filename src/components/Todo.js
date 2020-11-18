aab9c13fb0e2b8b3274b5b178fde0eefd8872918
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-18 22:23:36 +0500

    Code Updated

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState } from "react";
 
 let baseUrl = "fakeapi";
 
 export const Todos = () => {
+    let [ data, setData ] = useState([]);
 
-    useEffect(() => {
-      fetch(`/${baseUrl}/getTodos`)
+    const addTodoHandler = () => {
+        fetch(`/${baseUrl}/addTodos`, {
+            method: 'POST',
+            body: {id: 3742, text: "Hello From Rooshan"}
+        }).then(res => {
+            console.log('success',res)
+        }).catch(error => {
+            console.log('failed',error)
+        })
+    }
+
+    const getDataHandler = () => {
+        fetch(`/${baseUrl}/getTodos`)
         .then((res) => res.json())
-        .then((data) => console.log("data", data));
-    });
+        .then((data) => {
+            setData(data.todos)
+            console.log("data", data)
+        });
+    }
 
-    return <div>Hello from Todos</div>;
+    return(
+        <div>
+            <h1>Hello From Todos</h1>
+            <button onClick={addTodoHandler}>Add</button>
+            <button onClick={getDataHandler}>Get Todos</button>
+            <br />
+            {
+                JSON.stringify(data)
+            }
+        </div>
+    )
 };
--- src/mirage/index.js
+++ src/mirage/index.js
@@ -2,11 +2,17 @@ import { createServer } from 'miragejs'
 
 export default function() {
     let arr = [{ id: "1", text: "hello" }]
+    let users = [{ name: "Rooshan", age: 24 }]
     
     createServer({
         routes() {
             this.namespace = '/fakeapi'
             this.get('/getTodos', {todos: arr})
+            this.get('/getUsers', {todos: users})
+            this.post('/addTodos', (_,req) => {
+                console.log('req',req)
+                arr.push(req.requestBody)
+            })
         }
     })
 }
